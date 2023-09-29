d8c4abff8230664ac2378c42d0a84d866272c717
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-09-29 20:13:14 +0200

    new component Cart.vue

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,21 @@
       <button type="submit">Add Product</button>
     </form>
 
-    <!-- List Products -->
-    <h2>Products</h2>
-    <div v-for="product in products" :key="product.id">
-      <h3>{{ product.name }}</h3>
-      <p>Price: ${{ product.price }}</p>
-      <p>{{ product.description }}</p>
+  <!-- List Products -->
+  <h2>Products</h2>
+    <div class="product-grid">
+      <div v-for="product in products" :key="product.id" class="product-card">
+        <h3>{{ product.name }}</h3>
+        <p>Price: ${{ product.price }}</p>
+        <p>{{ product.description }}</p>
+        <button>Add To Cart</button>
+      </div>
     </div>
-    <div>
+
+    <div class="input-container">
       <input v-model="discountCode" placeholder="Enter Discount Code">
       <button @click="applyDiscount">Apply Discount</button>
     </div>
-    
   </div>
 </template>
 
@@ -37,15 +40,12 @@ export default {
       discountCode: "",
       products: []
     }
-
   },
  
-
   methods: {
     addProduct() {
       // Send the new product to the server to save in the database
       fetch('http://localhost/back-end-discount/createProduct.php', {
-        
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -92,39 +92,136 @@ export default {
     },
 
     applyDiscount() {
-  console.log(this.discountCode)
+      console.log(this.discountCode);
 
       fetch('http://localhost/back-end-discount/applyDiscount.php', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(this.discountCode),
+        body: JSON.stringify({ code: this.discountCode }),
       })
-
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         return response.json();
-
       })
       .then(data => {
-        console.log(data)});
-        console.log(this.discountCode)
-  
+        console.log(data);
+      });
     },
-  
- 
-  created() {
-    // Fetch the product list when the component is created
-    this.fetchProducts();
   },
 
-}}
-
+  mounted() {
+    // Fetch the product list when the component is mounted
+    this.fetchProducts();
+  },
+}
 </script>
 
 <style>
-/* Your CSS styles here */
+#app {
+  font-family: Arial, sans-serif;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+h2 {
+  font-size: 20px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+
+form {
+  margin-bottom: 20px;
+}
+
+form input,
+form textarea {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button[type="submit"],
+button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+button[type="submit"]:hover,
+button:hover {
+  background-color: #0056b3;
+}
+
+.product-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Adjust the column width as needed */
+  grid-gap: 20px; /* Adjust the gap between products */
+}
+
+.product-card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+}
+.product-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+.product-container h3 {
+  font-size: 18px;
+  margin: 0;
+}
+
+.product-container p {
+  font-size: 16px;
+  margin: 10px 0;
+}
+
+.input-container {
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.input-container input {
+  width: 70%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+.input-container button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+.input-container button:hover {
+  background-color: #0056b3;
+}
 </style>
+
